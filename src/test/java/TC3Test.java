da4112443836f0beec4898035e0c094dd052ba77
jackelinss <124317565+jackelinss@users.noreply.github.com>
2025-05-25 15:37:02 -0400

    Add files via upload
    primer commit

---
+++ src/test/java/TC3Test.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class TC3Test{
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void verificarInicioGoogle() {
+        driver.get("https://www.google.com");
+        WebElement boton = driver.findElement(By.name("btnK"));
+        Assert.assertEquals(boton.getAttribute("value"), "Buscar con Google");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
