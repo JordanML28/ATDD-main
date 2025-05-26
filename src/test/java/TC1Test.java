35351f84fd5bd23eaba5f175cd5fc8a2340ad8a8
JordanML28 <117130747+JordanML28@users.noreply.github.com>
2025-05-26 03:41:33 -0400

    Update TC1Test.java

--- src/test/java/TC1Test.java
+++ src/test/java/TC1Test.java
@@ -2,9 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+
 
 public class TC1Test {
     private WebDriver driver;
@@ -20,9 +24,10 @@ public class TC1Test {
     public void testLoginSuccess() throws InterruptedException {
         // Paso 1: Navegar a la página principal
         driver.get("http://localhost:5173/");
+        Thread.sleep(2000);
 
         // Paso 2: Verificar que el título de la página sea el esperado
-        assertEquals(driver.getTitle(), "Vue App");
+        assertEquals(driver.getTitle(), "login");
 
         // Paso 3: Obtener los campos de usuario y contraseña
         WebElement username = driver.findElement(By.id("username"));
@@ -30,20 +35,24 @@ public class TC1Test {
 
         // Paso 4: Ingresar el usuario y la contraseña válidos
         username.sendKeys("admin");
-        password.sendKeys("1234");
+        Thread.sleep(2000);
 
+        password.sendKeys("adminpass");
+        Thread.sleep(2000);
         // Paso 5: Verificar que los valores fueron ingresados correctamente
         assertEquals(username.getAttribute("value"), "admin");
-        assertEquals(password.getAttribute("value"), "1234");
+        assertEquals(password.getAttribute("value"), "adminpass");
 
         // Paso 6: Hacer clic en el botón de login
         WebElement loginBtn = driver.findElement(By.id("login-button"));
         loginBtn.click();
 
         // Paso 7: Esperar que se redireccione
-        Thread.sleep(1000);
-
+        
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         // Paso 8: Verificar que se redirige al dashboard
+        
+        wait.until(ExpectedConditions.urlContains("/dashboard"));
         assertTrue(driver.getCurrentUrl().contains("/dashboard"));
 
         // Paso 9: Verificar contenido de bienvenida en el dashboard
@@ -52,6 +61,7 @@ public class TC1Test {
 
         // Paso 10: Validar existencia del formulario
         assertTrue(driver.findElement(By.id("name")).isDisplayed());
+        Thread.sleep(4000);
     }
 
     @AfterTest
